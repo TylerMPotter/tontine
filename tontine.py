ad0fc5c3419eac5c4d830fee73553773d1b6ba1a
TylerMPotter <tpotter@colgate.edu>
2021-12-28 00:21:16 -0500

    adding functionality to click the checkin button

--- tontine.py
+++ tontine.py
@@ -47,7 +47,7 @@ while True:
         time.sleep(2)
 
         try:
-            driver.find_elements_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button').click()
+            driver.find_elements_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]').click()
 
             webhook = DiscordWebhook(url=os.environ['STREAKHOOK'], content="You've survived...for today...")
             response = webhook.execute()
