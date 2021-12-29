10287a20bf00a4d055cfa30b782e86d5e26b0e21
TylerMPotter <tpotter@colgate.edu>
2021-12-29 01:35:09 -0500

    try number 2, because I clicked the button by mistake on day 1

--- tontine.py
+++ tontine.py
@@ -11,6 +11,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ["GOOGLE_CHROME_BIN"]
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--disable-dev-ahm-usage")
 chrome_options.add_argument("--no-sandbox")
 
