f60021359020df53fc8f718147f047e2d981c567
TylerMPotter <tpotter@colgate.edu>
2021-12-29 02:32:02 -0500

    try number 2, because I clicked the button by mistake on day 1

--- tontine.py
+++ tontine.py
@@ -13,7 +13,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--window-size=1100,1000")
 chrome_options.add_argument("--disable-dev-ahm-usage")
-chrome_options.add_argument("--no-sandbox")]
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_experimental_option('useAutomationExtension', False)
 
