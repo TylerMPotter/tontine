730f55019cf71e05810f555de379679e4de57436
TylerMPotter <tpotter@colgate.edu>
2021-12-29 02:40:04 -0500

    Wowzers I think I outsmarted Captcha

--- tontine.py
+++ tontine.py
@@ -21,76 +21,64 @@ url = 'https://tontine.cash/'
 
 while True:
 
+    time.sleep(300)
+
     ua = UserAgent()
     userAgent = ua.random
     print(userAgent)
 
     chrome_options.add_argument("--user-agent=" + userAgent)
 
-#try:
+    try:
 
-    driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
+        driver = webdriver.Chrome(executable_path=os.environ["CHROMEDRIVER_PATH"], chrome_options=chrome_options)
 
-    driver.get(url)
+        driver.get(url)
 
-    data = {
-        'email': '//*[@id="__layout"]/div/div/div[4]/div[2]/div/input[1]',
-        'passw': '//*[@id="__layout"]/div/div/div[4]/div[2]/div/input[2]',
-    }
+        data = {
+            'email': '//*[@id="__layout"]/div/div/div[4]/div[2]/div/input[1]',
+            'passw': '//*[@id="__layout"]/div/div/div[4]/div[2]/div/input[2]',
+        }
 
-    time.sleep(5)
+        time.sleep(5)
 
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[1]/div[2]/button/div').click()
-    
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[1]/div[2]/button/div').click()
+        
 
-    time.sleep(2)
-
-    for key in data.keys():
         time.sleep(2)
-        info = "Placeholder"
-        if key == 'email':
-            info = email
-        if key == 'passw':
-            info = passw
 
-        driver.find_element_by_xpath(data.get(key)).send_keys(info)
-    
-    time.sleep(2)
+        for key in data.keys():
+            time.sleep(2)
+            info = "Placeholder"
+            if key == 'email':
+                info = email
+            if key == 'passw':
+                info = passw
 
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[4]/div[2]/div/button/div').click()
+            driver.find_element_by_xpath(data.get(key)).send_keys(info)
+        
+        time.sleep(2)
 
-    time.sleep(3)
+        driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[4]/div[2]/div/button/div').click()
 
-#try:
-    #driver.find_elements_by_xpath('//*[@id="__layout"]/div/div/div[5]/div[2]/div[1]/div')
-    #ac = ActionChains(driver)
-    #ac.move_to_element(elem).move_by_offset(1, 1).click().perform()
-    #driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[5]/div[1]').click()
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-    webhook = DiscordWebhook(url=os.environ['STREAKHOOK'], content="You've survived...for today...")
-    response = webhook.execute()
-#except:
-    text = driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[1]/div[1]/div[2]/div/div[1]').text
-    print("people left: " + text)
+        time.sleep(3)
+
+        try:
+            driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
+            webhook = DiscordWebhook(url=os.environ['STREAKHOOK'], content="You've survived...for today...")
+            response = webhook.execute()
+        except:
+            text = driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[1]/div[1]/div[2]/div/div[1]').text
+            print("people left: " + text)
 
 
-    time.sleep(2)
+        time.sleep(2)
 
-    driver.quit()
-#except:
-    webhook = DiscordWebhook(url=os.environ['LOGINHOOK'], content="something is broken")
-    response = webhook.execute()
-    driver.quit()
+        driver.quit()
+    except:
+        webhook = DiscordWebhook(url=os.environ['LOGINHOOK'], content="something is broken")
+        response = webhook.execute()
+        driver.quit()
 '''
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
