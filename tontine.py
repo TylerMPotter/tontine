170970d151af2e9cdd54b92ae6afee53d82ecaff
TylerMPotter <tpotter@colgate.edu>
2021-12-31 20:44:49 -0500

    Adding an alert which lets me know when a button was seen and pressed.

--- tontine.py
+++ tontine.py
@@ -3,6 +3,7 @@ from fake_useragent import UserAgent
 from discord_webhook import DiscordWebhook
 import time
 import os
+from datetime import datetime
 
 email = os.environ['EMAIL']
 passw = os.environ['PASSW']
@@ -19,9 +20,11 @@ chrome_options.add_experimental_option('useAutomationExtension', False)
 
 url = 'https://tontine.cash/'
 
+justSawButton = False
+
 while True:
 
-    time.sleep(300)
+    time.sleep(20)
 
     ua = UserAgent()
     userAgent = ua.random
@@ -63,12 +66,29 @@ while True:
 
         time.sleep(3)
 
+        if driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div'):
+
+            seen = datetime.now()
+            seenFormat = seen.strftime("%H:%M:%S")
+
+            justSawButton = True
+        
+        else:
+            if justSawButton:
+
+                clicked = datetime.now()
+                clikedFormat = clicked.strftime("%H:%M:%S")
+                
+                webhook = DiscordWebhook(url=os.environ['STREAKHOOK'], content="Button seen at: " + seenFormat + "\nButton clicked at: " + clikedFormat)
+                response = webhook.execute()
+
+                justSawButton[0] = False
+
+
         try:
             driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
             time.sleep(2)
             driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[3]/div/div[2]/button/div').click()
-            webhook = DiscordWebhook(url=os.environ['STREAKHOOK'], content="You've survived...for today...")
-            response = webhook.execute()
         except:
             text = driver.find_element_by_xpath('//*[@id="__layout"]/div/div/div[1]/div[1]/div[2]/div/div[1]').text
             print("people left: " + text)
